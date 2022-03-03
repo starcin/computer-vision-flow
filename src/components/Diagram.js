0c78fe2bec121c296af7e295b3f3f80cef8bc64f
starcin <starcin@gmail.com>
2022-03-03 18:14:18 +0300

    Added Undo

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -1,20 +1,5 @@
-import ReactFlow, {
-	Background,
-	Controls,
-	addEdge,
-	removeElements,
-	updateEdge,
-	getIncomers,
-} from "react-flow-renderer"
-import { useEffect, useState } from "react"
-import {
-	AlertDialog,
-	AlertDialogBody,
-	AlertDialogContent,
-	AlertDialogHeader,
-	AlertDialogOverlay,
-	Box,
-} from "@chakra-ui/react"
+import ReactFlow, { Background, Controls } from "react-flow-renderer"
+import { Box } from "@chakra-ui/react"
 import CustomEdge from "./CustomEdge"
 import ProblematicEdge from "./ProblematicEdge"
 import ConnectionLine from "./ConnectionLine"
@@ -36,163 +21,32 @@ const connectionLineStyle = { strokeWidth: "6px", stroke: "#319795" }
 
 export default function Diagram({
 	elements,
-	onFunctionNodeSelected,
-	setElements,
-	setParentRfInstance,
+	setReactFlowInstance,
+	onEdgeUpdate,
+	onEdgeButtonClick,
+	onElementsRemove,
+	onConnect,
 }) {
-	const [alertState, setAlertState] = useState({
-		isOpen: false,
-		sourceType: null,
-		targetType: null,
-	})
-	const [rfInstance, setRfInstance] = useState(null)
-
-	function setReactFlowInstances(instance) {
-		setParentRfInstance(instance)
-		setRfInstance(instance)
-	}
-
-	// Triggered when the alert is closed. Resets the state. (Not entirely necessary)
-	const onAlertClose = () =>
-		setAlertState({
-			isOpen: false,
-			sourceType: null,
-			targetType: null,
-		})
-
-	//Triggered when en element is clicked (not active at the moment)
-	function onElementClick(event, element) {
-		if (element.type === "functionNode") {
-			onFunctionNodeSelected(element)
-		}
-	}
-
-	// Triggered when connection completed. Chooses the correct edge type depending on the handle compatibility.
-	const onConnect = (params) => {
-		// Check if the target is already has a connection
-		if (
-			getIncomers(
-				rfInstance
-					.toObject()
-					.elements.find((element) => element.id === params.target),
-				elements
-			).length === 0
-		) {
-			// Check if types match
-			if (
-				params.sourceHandle === params.targetHandle ||
-				params.sourceHandle === "any" ||
-				params.targetHandle === "any"
-			) {
-				setElements((els) =>
-					addEdge(
-						{
-							...params,
-							type: "custom",
-							data: { onButtonClick: onEdgeButtonClick },
-						},
-						els
-					)
-				)
-			} else {
-				setAlertState({
-					isOpen: true,
-					sourceType: params.sourceHandle,
-					targetType: params.targetHandle,
-				})
-				// setElements((els) => addEdge({ ...params, type: "problematic" }, els))
-				// setElements((els) => addEdge({ ...params, animated: true }, els))
-			}
-		}
-	}
-
-	// Triggered when the connection is created but still floating
-	const onConnectStart = (param1, param2) => {
-		// console.log("1", param1)
-		// console.log("2", param2)
-	}
-
-	// Triggered when the connection of the edge is moved to another node
-	const onEdgeUpdate = (oldEdge, newConnection) => {
-		if (newConnection.sourceHandle === newConnection.targetHandle) {
-			setElements((els) =>
-				updateEdge({ ...oldEdge, type: "custom" }, newConnection, els)
-			)
-		} else {
-			setElements((els) =>
-				updateEdge({ ...oldEdge, type: "problematic" }, newConnection, els)
-			)
-		}
-	}
-
-	// Deletes edge. Triggered when the x on edge is clicked
-	function onEdgeButtonClick(event, id) {
-		setElements((els) =>
-			removeElements([els.find((element) => element.id === id)], els)
-		)
-	}
-
-	// Deletes element. Triggered when the deleteKeyCode is pressed
-	const onElementsRemove = (elementsToRemove) =>
-		setElements((els) => removeElements(elementsToRemove, els))
-
-	// function onDiagramClicked() {
-	// 	const selecteds = rfInstance
-	// 		.getElements()
-	// 		.filter((element) => element.data.isSelected === true)
-	// 	console.log(selecteds.length)
-	// }
-
 	return (
-		<Box
-			height="700px"
-			width="100%"
-			borderRadius="7px"
-			border="2px"
-			borderColor="silver"
-			overflow="hidden"
-			margin="0px 20px"
-		>
+		<Box height="700px" width="100%">
 			<ReactFlow
 				elements={elements}
 				nodeTypes={nodeTypes}
 				edgeTypes={edgeTypes}
 				selectNodesOnDrag={false}
 				elementsSelectable={true}
-				// onElementClick={onElementClick}
 				onElementsRemove={onElementsRemove}
-				onLoad={setReactFlowInstances}
+				onLoad={setReactFlowInstance}
 				onConnect={onConnect}
-				onConnectStart={onConnectStart}
 				onEdgeUpdate={onEdgeUpdate}
 				style={{ background: "white" }}
 				snapToGrid={true}
 				connectionLineComponent={ConnectionLine}
 				deleteKeyCode={46} /* 'delete'-key */
-				// onClick={onDiagramClicked}
 			>
 				<Controls />
 				<Background />
 			</ReactFlow>
-			<AlertDialog
-				isOpen={alertState.isOpen}
-				// leastDestructiveRef={cancelRef}
-				onClose={onAlertClose}
-			>
-				<AlertDialogOverlay>
-					<AlertDialogContent>
-						<AlertDialogHeader fontSize="lg" fontWeight="bold">
-							Data types are incompatible
-						</AlertDialogHeader>
-
-						<AlertDialogBody>
-							{/* {`The source is ${alertState.sourceType} but the target expects ${alertState.targetType}`} */}
-							The source is <strong>{alertState.sourceType}</strong> but the
-							target expects <strong>{alertState.targetType}</strong>
-						</AlertDialogBody>
-					</AlertDialogContent>
-				</AlertDialogOverlay>
-			</AlertDialog>
 		</Box>
 	)
 }
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,9 +2,25 @@ import FunctionList from "./FunctionList"
 import SettingsPanel from "./SettingsPanel"
 import Diagram from "./Diagram"
 import { useEffect, useState } from "react"
-import { Flex } from "@chakra-ui/react"
+import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogContent,
+	AlertDialogHeader,
+	AlertDialogOverlay,
+	Box,
+	Button,
+	Flex,
+	HStack,
+	VStack,
+} from "@chakra-ui/react"
 import SideBar from "./SideBar"
-import { removeElements } from "react-flow-renderer"
+import {
+	removeElements,
+	updateEdge,
+	getIncomers,
+	addEdge,
+} from "react-flow-renderer"
 
 const initialElements = [
 	{
@@ -32,6 +48,7 @@ const initialElements = [
 ]
 
 const defaultNodeOffsetX = 160
+const maxUndo = 50
 
 export default function Editor() {
 	const [functions, setFunctions] = useState([])
@@ -39,7 +56,22 @@ export default function Editor() {
 	const [elements, setElements] = useState([])
 	const [lastUsedId, setLastUsedId] = useState(0)
 	const [rfInstance, setRfInstance] = useState(null)
+	const [undoElementsStates, setUndoElementsStates] = useState([])
+	const [alertState, setAlertState] = useState({
+		isOpen: false,
+		sourceType: null,
+		targetType: null,
+	})
 
+	// Gets triggered when the alert is closed. Resets the state. (Not entirely necessary)
+	const onAlertClose = () =>
+		setAlertState({
+			isOpen: false,
+			sourceType: null,
+			targetType: null,
+		})
+
+	// Load all the functions from json
 	useEffect(() => {
 		fetch("databases/functions.json", {
 			headers: {
@@ -55,35 +87,7 @@ export default function Editor() {
 			})
 	}, [])
 
-	function onFunctionNodeSelected(element) {
-		setActiveElement(element)
-	}
-
-	function onNodeSelectedChanged(isSelected, id, data) {
-		if (isSelected) {
-			setActiveElement({ id, data })
-		} else {
-			if (
-				rfInstance
-					.toObject()
-					.elements.find(
-						(element) =>
-							"data" in element &&
-							"isSelected" in element.data &&
-							element.data.isSelected === true
-					) === undefined
-			) {
-				setActiveElement(null)
-			}
-		}
-	}
-
-	function onDeleteClicked(event, id) {
-		setElements((els) =>
-			removeElements([els.find((element) => element.id === id)], els)
-		)
-	}
-
+	// Calculates a suitable Y coordinate to put a new node
 	function getEmptySpace() {
 		const flowElements = rfInstance.toObject().elements
 		for (let y = 20; y < 1000; y = y + 100) {
@@ -97,7 +101,9 @@ export default function Editor() {
 		}
 	}
 
+	// Adds new node
 	function addNode(funcObj) {
+		updateUndoList()
 		if (funcObj.id === "input") {
 			const newNode = {
 				id: (lastUsedId + 1).toString(),
@@ -155,10 +161,40 @@ export default function Editor() {
 		}
 	}
 
+	function updateUndoList() {
+		setUndoElementsStates((previousState) =>
+			previousState.length < maxUndo
+				? previousState.concat([rfInstance.toObject().elements])
+				: previousState.slice(1).concat([rfInstance.toObject().elements])
+		)
+	}
+
+	// Gets triggered when a node's internal "selected" property is changed
+	function onNodeSelectedChanged(isSelected, id, data) {
+		if (isSelected) {
+			setActiveElement({ id, data })
+		} else {
+			if (
+				rfInstance
+					.toObject()
+					.elements.find(
+						(element) =>
+							"data" in element &&
+							"isSelected" in element.data &&
+							element.data.isSelected === true
+					) === undefined
+			) {
+				setActiveElement(null)
+			}
+		}
+	}
+
+	// Gets triggered when a function name is clicked on the menu
 	function onFunctionClicked(funcObj) {
 		addNode(funcObj)
 	}
 
+	// Gets triggered when a module is selected on the side bar
 	function onModuleSelected(module) {
 		console.log(module)
 		setElements(
@@ -171,6 +207,93 @@ export default function Editor() {
 		)
 	}
 
+	// Gets triggered when connection completed. Chooses the correct edge type depending on the handle compatibility.
+	const onConnect = (params) => {
+		// Check if the target is already has a connection
+		if (
+			getIncomers(
+				rfInstance
+					.toObject()
+					.elements.find((element) => element.id === params.target),
+				elements
+			).length === 0
+		) {
+			// Check if types match
+			if (
+				params.sourceHandle === params.targetHandle ||
+				params.sourceHandle === "any" ||
+				params.targetHandle === "any"
+			) {
+				updateUndoList()
+				setElements((els) =>
+					addEdge(
+						{
+							...params,
+							type: "custom",
+							data: { onButtonClick: onEdgeButtonClick },
+						},
+						els
+					)
+				)
+			} else {
+				setAlertState({
+					isOpen: true,
+					sourceType: params.sourceHandle,
+					targetType: params.targetHandle,
+				})
+				// setElements((els) => addEdge({ ...params, type: "problematic" }, els))
+				// setElements((els) => addEdge({ ...params, animated: true }, els))
+			}
+		}
+	}
+
+	// Sent to custom nodes by addNode function. Gets triggered when x is clicked or delete key is pressed.
+	function onDeleteClicked(event, id) {
+		updateUndoList()
+		setElements((els) =>
+			removeElements([els.find((element) => element.id === id)], els)
+		)
+	}
+
+	// Deletes edge. Gets triggered when the x on edge is clicked
+	const onEdgeButtonClick = (event, id) => {
+		updateUndoList()
+		setElements((els) =>
+			removeElements([els.find((element) => element.id === id)], els)
+		)
+	}
+
+	// Deletes element. Gets triggered when the deleteKeyCode is pressed
+	const onElementsRemove = (elementsToRemove) => {
+		updateUndoList()
+		setElements((els) => removeElements(elementsToRemove, els))
+	}
+
+	// Gets triggered when the connection of the edge is moved to another node
+	const onEdgeUpdate = (oldEdge, newConnection) => {
+		updateUndoList()
+		if (newConnection.sourceHandle === newConnection.targetHandle) {
+			setElements((els) =>
+				updateEdge({ ...oldEdge, type: "custom" }, newConnection, els)
+			)
+		} else {
+			setElements((els) =>
+				updateEdge({ ...oldEdge, type: "problematic" }, newConnection, els)
+			)
+		}
+	}
+
+	function onUndoClicked() {
+		if (undoElementsStates[undoElementsStates.length - 1]) {
+			setElements(undoElementsStates[undoElementsStates.length - 1])
+			setUndoElementsStates((previousState) => previousState.slice(0, -1))
+		} else {
+			console.log("no undo")
+		}
+	}
+
+	function onRedoClicked() {}
+
 	return (
 		<Flex
 			className="editor-container"
@@ -181,14 +304,52 @@ export default function Editor() {
 			maxWidth="1400px"
 		>
 			<FunctionList functions={functions} onItemSelected={onFunctionClicked} />
-			<Diagram
-				elements={elements}
-				// onFunctionNodeSelected={onFunctionNodeSelected}
-				setElements={setElements}
-				setParentRfInstance={setRfInstance}
-			/>
+			<VStack
+				width="100%"
+				margin="0px 20px"
+				borderRadius="7px"
+				border="2px"
+				borderColor="silver"
+				overflow="hidden"
+			>
+				<HStack w="100%" justify="start" p="2px 4px">
+					<Button onClick={onUndoClicked} size="xs">
+						Undo
+					</Button>
+					<Button onClick={onRedoClicked} size="xs">
+						Redo
+					</Button>
+				</HStack>
+
+				<Diagram
+					elements={elements}
+					setReactFlowInstance={setRfInstance}
+					onEdgeUpdate={onEdgeUpdate}
+					onElementsRemove={onElementsRemove}
+					onConnect={onConnect}
+				/>
+			</VStack>
 
 			<SideBar element={activeElement} onModuleSelected={onModuleSelected} />
+			<AlertDialog
+				isOpen={alertState.isOpen}
+				// leastDestructiveRef={cancelRef}
+				onClose={onAlertClose}
+			>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader fontSize="lg" fontWeight="bold">
+							Data types are incompatible
+						</AlertDialogHeader>
+
+						<AlertDialogBody>
+							{/* {`The source is ${alertState.sourceType} but the target expects ${alertState.targetType}`} */}
+							The source is <strong>{alertState.sourceType}</strong> but the
+							target expects <strong>{alertState.targetType}</strong>
+						</AlertDialogBody>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
 		</Flex>
 	)
 }
